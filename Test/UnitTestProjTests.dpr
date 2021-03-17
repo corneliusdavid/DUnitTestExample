f9ce6e76406c1b9b691f29be920f3b1537123443
dev@corneliusconcepts.com <corneliusdavid>
2021-03-17 16:27:02 -0700

    added test project

---
+++ Test/UnitTestProjTests.dpr
@@ -0,0 +1,27 @@
+program UnitTestProjTests;
+{
+
+  Delphi DUnit Test Project
+  -------------------------
+  This project contains the DUnit test framework and the GUI/Console test runners.
+  Add "CONSOLE_TESTRUNNER" to the conditional defines entry in the project options
+  to use the console test runner.  Otherwise the GUI test runner will be used by
+  default.
+
+}
+
+{$IFDEF CONSOLE_TESTRUNNER}
+{$APPTYPE CONSOLE}
+{$ENDIF}
+
+uses
+  DUnitTestRunner,
+  TestuPersonClass in 'TestuPersonClass.pas',
+  uPersonClass in '..\uPersonClass.pas';
+
+{$R *.RES}
+
+begin
+  DUnitTestRunner.RunRegisteredTests;
+end.
+
